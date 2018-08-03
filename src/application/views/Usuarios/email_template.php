967de4b3f8b1baf625ab492b322a695b0a466631
Raphael Ramos <raphael.ramos@opensource.com>
2018-08-03 18:02:20 -0300

    Template Email

--- src/application/views/Usuarios/email_template.php
+++ src/application/views/Usuarios/email_template.php
@@ -3,7 +3,6 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<title>Requisição de senha</title>
-		<link rel="stylesheet" href="<?=base_url('assets/css/email.css')?>">
 	</head>
 	<body>
 		<div class="box_message">
@@ -13,9 +12,9 @@
 			<div class="body_box">
 				<p>Vimos que você solicitou uma alteração de senha.</p>
 				<p>Seu código de ativação é: </p>
-				<span class="acces_code"><?=$code_access?></span>
+				<input id="content" style="border:none;" type="text" value="<?=$code_access?>" readonly>
 			</div>
-			<footer>
+			<footer>	
 				<p>Atenciosamente, <br> Equipe do sistema Psi (Prontuários Eletrônicos em um Sistema Inteligente)</p>  
 			</footer>
 		</div>
