af0f9661ce3f1b46f120b7c19236c0a1b7a05b83
raphaelramosss <raphael.opensource@gmail.com>
2018-09-12 19:33:25 -0300

    Transaction de Horários

--- src/application/models/HorariosModel.php
+++ src/application/models/HorariosModel.php
@@ -5,6 +5,16 @@ class HorariosModel extends CI_Model
 {
     public function add($dados)
     {
+        $this->db->trans_start();
         $this->db->insert('horario',$dados);
+        $this->db->trans_complete();
+        if ($this->db->trans_status() === FALSE)
+        {
+            $this->db->trans_rollback();
+        }
+        else
+        {
+            $this->db->trans_commit();
+        }
     }
 }
