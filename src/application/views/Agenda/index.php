0db9a8b172c403042b6229497da90df4a530319f
Raphael Ramos <raphael.opensource@gmail.com>
2018-09-25 18:56:01 -0300

    Concluíndo

--- src/application/views/Agenda/index.php
+++ src/application/views/Agenda/index.php
@@ -80,13 +80,13 @@
 						<div class="form-group">
 							<label for="inputEmail3" class="col-sm-2 control-label">Data/Hora Inicial</label>
 							<div class="col-sm-10">
-								<input type="text" class="form-control" name="start" id="start">
+								<input type="text" class="form-control" name="start" id="start" onKeyPress="DataHora(event, this)">
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="inputEmail3" class="col-sm-2 control-label">Data/Hora Final</label>
 							<div class="col-sm-10">
-								<input type="text" class="form-control" name="end" id="end">
+								<input type="text" class="form-control" name="end" id="end" onKeyPress="DataHora(event, this)">
 							</div>
 						</div>
 						<div class="form-group">
@@ -156,6 +156,37 @@
 		$('.visualizar').slideToggle()
 	})
 
+	function DataHora(evento, objeto){
+		var keypress=(window.event)?event.keyCode:evento.which;
+		campo = eval (objeto);
+		if (campo.value == '00/00/0000 00:00:00'){
+			campo.value=""
+		}
+	 
+		caracteres = '0123456789';
+		separacao1 = '/';
+		separacao2 = ' ';
+		separacao3 = ':';
+		conjunto1 = 2;
+		conjunto2 = 5;
+		conjunto3 = 10;
+		conjunto4 = 13;
+		conjunto5 = 16;
+		if ((caracteres.search(String.fromCharCode (keypress))!=-1) && campo.value.length < (19)){
+			if (campo.value.length == conjunto1 )
+			campo.value = campo.value + separacao1;
+			else if (campo.value.length == conjunto2)
+			campo.value = campo.value + separacao1;
+			else if (campo.value.length == conjunto3)
+			campo.value = campo.value + separacao2;
+			else if (campo.value.length == conjunto4)
+			campo.value = campo.value + separacao3;
+			else if (campo.value.length == conjunto5)
+			campo.value = campo.value + separacao3;
+		}else{
+			event.returnValue = false;
+		}
+	}
 
 </script>
 
