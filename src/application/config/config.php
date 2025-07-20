d7216b0572c9f35e15525e3621de0d65a7c4bcb8
Raphael Ramos <raphael.opensource@gmail.com>
2025-07-19 21:40:38 -0300

    fix: rules on apache rewrite module

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$config['base_url'] = 'http://localhost/psi/src';
+$config['base_url'] = 'http://localhost:9001';
 //$config['base_url'] = 'https://prontuariospsi.000webhostapp.com/';
 
 $config['index_page'] = '';
@@ -336,7 +336,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = '/tmp';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
